33f05213633a9a2ec3460712a9557caed91c67f4
DePacos <depacos@yandex.ru>
2024-04-09 23:57:29 +0300

    added react router dom

--- src/layout/article/Article.tsx
+++ src/layout/article/Article.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import {S} from './Article_Styles'
 import {Posts} from "../posts/Posts";
 import {PostsType} from "../../App";
+import {Messages} from "../messages/Messages";
+import {Profile} from "../profile/Profile";
+
+import {Routes, Route} from 'react-router-dom'
 
 type ArticleProps = {
     posts: Array<PostsType>
 }
 
-export const Article = ({posts}:ArticleProps) => {
+export const Article = ({posts}: ArticleProps) => {
     return (
         <S.Article>
-            <h1>Title</h1>
-            <Posts posts={posts}/>
+            <Routes>
+                <Route path="/posts" element={<Posts posts={posts} />}/>
+                <Route path="/messages" element={<Messages/>}/>
+                <Route path="/profile" element={<Profile />}/>
+            </Routes>
         </S.Article>
     );
 };
