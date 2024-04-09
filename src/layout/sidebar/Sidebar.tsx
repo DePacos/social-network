e7e287cd517f49f8cdafd5cd9fd5931e02b7f7c4
DePacos <depacos@yandex.ru>
2024-04-09 23:59:33 +0300

    added react router dom, link

--- src/layout/sidebar/Sidebar.tsx
+++ src/layout/sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import {S} from './Sidebar_Styles'
+
 export const Sidebar = () => {
     return (
         <S.Sidebar>
         <nav>
             <ul>
-                <li><a href="/#">Main</a></li>
-                <li><a href="/#">News</a></li>
-                <li><a href="/#">Message</a></li>
-                <li><a href="/#">Profile</a></li>
-                <li><a href="/#">Setting</a></li>
+                <li><Link to="/">Main</Link></li>
+                <li><Link to="/posts">Posts</Link></li>
+                <li><Link to="/messages">Message</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/#">Setting</Link></li>
             </ul>
         </nav>
         </S.Sidebar>
