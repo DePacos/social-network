1677ad5e81ef417a0ad0f3008e961c06edbd9bbc
DePacos <depacos@yandex.ru>
2024-04-21 23:14:25 +0300

    added props posts, changeAddPostInput, addPostInput, and addPost for Profile

--- src/layout/article/Article.tsx
+++ src/layout/article/Article.tsx
@@ -9,15 +9,26 @@ import {Routes, Route} from 'react-router-dom'
 
 type ArticleProps = {
     posts: Array<PostsType>
+    changeAddPostInput: (value:string) => void
+    addPostInput: string
+    addPost: () => void
 }
 
-export const Article = ({posts}: ArticleProps) => {
+export const Article = ({posts, changeAddPostInput, addPostInput, addPost}: ArticleProps) => {
+
+    const profilePosts = posts.filter(post => post.userId === 3)
+
     return (
         <S.Article>
             <Routes>
                 <Route path="/posts" element={<Posts posts={posts} />}/>
                 <Route path="/messages" element={<Messages/>}/>
-                <Route path="/profile" element={<Profile />}/>
+                <Route path="/profile" element={<Profile
+                    posts={profilePosts}
+                    changeAddPostInput={changeAddPostInput}
+                    addPostInput={addPostInput}
+                    addPost={addPost}
+                />}/>
             </Routes>
         </S.Article>
     );
