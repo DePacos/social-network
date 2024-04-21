27a0ed05ab1dd543b1454ed8f6d78df0a547dde1
DePacos <depacos@yandex.ru>
2024-04-21 23:16:03 +0300

    added ProfileProps and render posts

--- src/layout/profile/Profile.tsx
+++ src/layout/profile/Profile.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-
+import React, {ChangeEvent} from 'react';
 import {S} from './Profile_Styles'
+import {Button} from "../../components/button/Button";
+import {PostsType} from "../../App";
 
+type ProfileProps = {
+    posts: Array<PostsType>
+    changeAddPostInput: (value: string) => void
+    addPostInput: string
+    addPost: () => void
+}
 
-export const Profile = () => {
+export const Profile = ({posts, changeAddPostInput, addPostInput, addPost}:ProfileProps) => {
+
+    const buttonHandler = () => {
+        addPost()
+    }
+    const textAriaHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        changeAddPostInput(e.currentTarget.value)
+    }
+
+    const renderPost = posts.map(post =>
+            <li key={post.id}>
+                <h2><a href="/#">{post.title}</a></h2>
+                <p>{post.body}</p>
+                <img src={post.img} alt="post-img"/>
+            </li>
+    )
+
     return (
         <S.Profile>
             <h1>Profile</h1>
-            <p>Profile Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam beatae distinctio eos facere id ipsam molestias perspiciatis quam quo quos, rerum saepe! Deserunt dolore eligendi eveniet harum pariatur voluptates!</p>
+            <S.Post>{renderPost}</S.Post>
+            <S.WrapAddPost>
+            <textarea value={addPostInput} onChange={(e) => textAriaHandler(e)}/>
+                <Button title="Add Post" callback={buttonHandler}/>
+            </S.WrapAddPost>
+
         </S.Profile>
     );
 };
