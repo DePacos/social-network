b46c8344c9e7b77687e13d1ccb3eb96e18320081
DePacos <depacos@yandex.ru>
2024-04-29 23:57:26 +0300

    imposition component Messages

--- src/layout/messages/Messages.tsx
+++ src/layout/messages/Messages.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
 import {S} from './Messages_Styles.ts'
+import {NavLink} from "react-router-dom";
+import avatar from '../../assets/images/avatar-dead-monster.svg'
+import {Button} from "../../components/button/Button";
 
 
 export const Messages = () => {
     return (
         <S.Messages>
             <h1>Messages</h1>
-            <p>Messages Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias amet asperiores atque debitis doloribus et facilis laborum maiores modi molestiae perspiciatis praesentium quae quas quasi quibusdam, rem sapiente similique.</p>
+            <S.MessagesWrap>
+                <S.Contacts>
+                    <ul>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Bob</NavLink></li>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Max</NavLink></li>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Alex</NavLink></li>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Dan</NavLink></li>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>David</NavLink></li>
+                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Val</NavLink></li>
+                    </ul>
+                </S.Contacts>
+                <S.MessagesView>
+                    <S.MessagesCurrentUser>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    </S.MessagesCurrentUser>
+                    <S.MessagesContact className="messages_contact-user">
+                        A, autem ipsum iure ratione rem sit.
+                    </S.MessagesContact>
+                </S.MessagesView>
+                <S.MessagesSend>
+                    <textarea/>
+                    <Button title={'Send'} callback={() => {}}/>
+                </S.MessagesSend>
+            </S.MessagesWrap>
         </S.Messages>
     );
 };
--- src/layout/messages/Messages_Styles.ts.tsx
+++ src/layout/messages/Messages_Styles.ts.tsx
@@ -5,6 +5,90 @@ const Messages = styled.div
     `
     `
 
+const MessagesWrap = styled.div
+    `
+        display: grid;
+        grid-template-columns: 300px 1fr;
+        grid-template-rows: 1fr 70px;
+        margin-bottom: 30px;
+    `
+
+const Contacts = styled.div
+    `
+        grid-row: 1 / 3;
+        li + li {
+            margin-top: 20px;
+        }
+
+        a {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            width: max-content;
+            transition: transform .3s;
+        }
+
+        a:hover {
+            transform: scale(1.05);
+        }
+    `
+
+const MessagesCurrentUser = styled.div
+    `
+        padding: 10px 15px;
+        background-color: cadetblue;
+        height: max-content;
+        justify-self: end;
+        border-radius: 10px;
+    `
+const MessagesContact = styled.div
+    `
+        padding: 10px 15px;
+        background-color: #0c9bd4;
+        height: max-content;
+        justify-self: start;
+        border-radius: 10px;
+    `
+
+const MessagesView = styled.div
+    `
+        display: grid;
+        align-content: end;
+        gap: 20px;
+    `
+
+const MessagesSend = styled.div
+    `
+        display: grid;
+        grid-template-columns: 80% 20%;
+        gap: 20px;
+        grid-row: 2 / 3;
+        padding-top: 30px;
+
+        textarea {
+            padding: 5px 10px;
+            border-radius: 10px;
+            overflow: hidden;
+            resize: none;
+        }
+
+        button {
+            background-color: #5f9ea0;
+            border-radius: 10px;
+            transition: transform .3s;
+        }
+
+        button:hover {
+            transform: scale(1.05);
+        }
+    `
+
 export const S = {
-    Messages
+    Messages,
+    MessagesWrap,
+    MessagesView,
+    MessagesContact,
+    MessagesCurrentUser,
+    MessagesSend,
+    Contacts,
 }
