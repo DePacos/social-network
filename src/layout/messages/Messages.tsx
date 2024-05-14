cd73da30e7b0c199c0046ce746517aaff9951cff
DePacos <depacos@yandex.ru>
2024-05-14 23:21:45 +0300

    changing the rendering of data-based links

--- src/layout/messages/Messages.tsx
+++ src/layout/messages/Messages.tsx
@@ -1,38 +1,29 @@
 import React from 'react';
 
-import {S} from './Messages_Styles.ts'
-import {NavLink} from "react-router-dom";
-import avatar from '../../assets/images/avatar-dead-monster.svg'
-import {Button} from "../../components/button/Button";
+import {S} from './Messages_Styles'
+import {NavLink, Outlet} from "react-router-dom";
+import {UsersType} from "../../App";
+
+type MessagesProps = {
+    users: Array<UsersType>
+}
 
 
-export const Messages = () => {
+export const Messages = ({users}: MessagesProps) => {
     return (
         <S.Messages>
             <h1>Messages</h1>
             <S.MessagesWrap>
                 <S.Contacts>
                     <ul>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Bob</NavLink></li>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Max</NavLink></li>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Alex</NavLink></li>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Dan</NavLink></li>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>David</NavLink></li>
-                        <li><NavLink to={'#'}><img src={avatar} alt="avatar"/>Val</NavLink></li>
+                        {users.map(user => <li key={user.id}>
+                            <NavLink to={user.massagesId}><img
+                                src={user.avatar}
+                                alt="avatar"/>{user.name}
+                            </NavLink></li>)}
                     </ul>
                 </S.Contacts>
-                <S.MessagesView>
-                    <S.MessagesCurrentUser>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    </S.MessagesCurrentUser>
-                    <S.MessagesContact className="messages_contact-user">
-                        A, autem ipsum iure ratione rem sit.
-                    </S.MessagesContact>
-                </S.MessagesView>
-                <S.MessagesSend>
-                    <textarea/>
-                    <Button title={'Send'} callback={() => {}}/>
-                </S.MessagesSend>
+                <Outlet/>
             </S.MessagesWrap>
         </S.Messages>
     );
