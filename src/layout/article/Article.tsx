22d7129fb9ff8e8417734beb51c7998a8c844f14
DePacos <depacos@yandex.ru>
2024-05-14 23:15:11 +0300

    added users and message types in ArticleProps change routing for user messages

--- src/layout/article/Article.tsx
+++ src/layout/article/Article.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import {S} from './Article_Styles'
 import {Posts} from "../posts/Posts";
-import {PostsType} from "../../App";
+import {MessagesType, PostsType, UsersType} from "../../App";
 import {Messages} from "../messages/Messages";
 import {Profile} from "../profile/Profile";
 
 import {Routes, Route} from 'react-router-dom'
+import {MessagesView} from "../messages/MessagesView";
+
 
 type ArticleProps = {
     posts: Array<PostsType>
+    users: Array<UsersType>
+    messages: MessagesType
     changeAddPostInput: (value:string) => void
     addPostInput: string
     addPost: () => void
 }
 
-export const Article = ({posts, changeAddPostInput, addPostInput, addPost}: ArticleProps) => {
+export const Article = ({posts, users, messages, changeAddPostInput, addPostInput, addPost}: ArticleProps) => {
 
     const profilePosts = posts.filter(post => post.userId === 3)
 
     return (
         <S.Article>
             <Routes>
+                <Route path="/" element={<h1>Main</h1>}/>
                 <Route path="/posts" element={<Posts posts={posts} />}/>
-                <Route path="/messages" element={<Messages/>}/>
+                <Route path="/messages" element={<Messages users={users}  />}>
+                    <Route path=':id' element={<MessagesView messages={messages}/>}/>
+                </Route>
                 <Route path="/profile" element={<Profile
                     posts={profilePosts}
                     changeAddPostInput={changeAddPostInput}
                     addPostInput={addPostInput}
                     addPost={addPost}
                 />}/>
+                <Route path="/setting" element={<h1>Setting</h1>}/>
             </Routes>
         </S.Article>
     );
