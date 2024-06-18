2157e71357f76c8db005515126838a89c760a847
DeBook <depacos1@gmail.com>
2024-06-18 22:23:08 +0300

    added style for active nav link

--- src/layout/sidebar/Sidebar_Styles.ts
+++ src/layout/sidebar/Sidebar_Styles.ts
@@ -3,10 +3,23 @@ import styled from "styled-components";
 
 const Sidebar = styled.aside
 `
-padding: 40px 20px 20px 0;
+    padding: 40px 20px 20px 0;
 
     nav {
         font-size: 18px;
+
+        .active:after {
+            content: '';
+            display: inline-block;
+            position: absolute;
+            bottom: -5px;
+            left: 0;
+            background-color: currentColor;
+            height: 2px;
+            width: 100%;
+            border-radius: 10px;
+            transition: width 0.3s;
+        }
     }
 
     ul {
@@ -25,10 +38,12 @@ padding: 40px 20px 20px 0;
             transition: width 0.3s;
         }
     }
-    a{
+
+    a {
         position: relative;
         padding: 0 2px;
     }
+
     a:after {
         content: '';
         display: inline-block;
@@ -41,7 +56,8 @@ padding: 40px 20px 20px 0;
         border-radius: 10px;
         transition: width 0.3s;
     }
-    a:hover:after{
+
+    a:hover:after {
         width: 100%;
     }
 `
