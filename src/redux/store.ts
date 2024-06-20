23f7a6e0175a33d9cdbb4ce269859dac1f6355d0
DeBook <depacos1@gmail.com>
2024-06-20 23:49:21 +0300

    added reducers dialogs and messages

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {postReducer} from "./reducers/postReducer";
+import {dialogsReducer} from "./reducers/dialogsReducer";
+import {messagesReducer} from "./reducers/messagesReducer";
 
 
 
 export const rootReducer = combineReducers({
-    posts: postReducer
+    posts: postReducer,
+    dialogs: dialogsReducer,
+    messages: messagesReducer
 })
 
 export const store= legacy_createStore(rootReducer)
