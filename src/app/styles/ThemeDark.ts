1b54c7d0e2c3c4b9b155893db443dc26850c91dd
DeBook <depacos1@gmail.com>
2024-08-24 22:01:00 +0300

    moved to \src\app\styles

---
+++ src/app/styles/ThemeDark.ts
@@ -0,0 +1,10 @@
+import login from "../../shared/assets/icons/login-icon-dark.svg"
+export const socialThemeDark = {
+  colors: {
+    body: "#3d3b3a",
+    fontColor: "#fff",
+  },
+  icons: {
+    loginIcon: login,
+  },
+}
---
+++ src/app/styles/ThemeLight.ts
@@ -0,0 +1,11 @@
+import login from "../../shared/assets/icons/login-icon-light.svg"
+
+export const socialThemeLight = {
+  colors: {
+    body: "#e7e7e7",
+    fontColor: "#222",
+  },
+  icons: {
+    loginIcon: login,
+  },
+}
