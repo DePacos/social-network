4f9ff233ff858243d75e1ca6f8e1f0dfb1ccc210
DeBook <depacos1@gmail.com>
2024-08-28 23:22:39 +0300

    create routers

---
+++ src/app/routers/routers.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { createBrowserRouter, Navigate } from "react-router-dom"
+import { App } from "../App"
+import { ErrorPage } from "../../pages/errorPage/ErrorPage"
+import Profile from "../../pages/profile/Profile"
+import Messages from "../../features/messages/Messages"
+import Users from "../../pages/users/Users"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Navigate to="/404" />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/" />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/messages",
+        element: <Messages />,
+      },
+    ],
+  },
+  {
+    path: "404",
+    element: <ErrorPage />,
+  },
+])
