6fd294ab5a2987e1ecf07454833f14432bccad1d
DeBook <depacos1@gmail.com>
2024-09-02 23:29:23 +0300

    added select quantity users on page and added types for props

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,22 +1,31 @@
-import React, { Dispatch } from "react"
-import { AppRootState, User, UserType } from "../../app/types/types"
+import React from "react"
+import { AppRootState, User } from "../../app/types/types"
 import { connect } from "react-redux"
 import { S } from "./Users_Styles"
-import { usersSlice } from "../../entities/users/usersSlice"
+import { changePageItems, fetchUsers } from "../../entities/users/usersReducer"
+import Pagination from "./Pagination"
 
 class Users extends React.Component<Props> {
   componentDidMount() {
-    this.props.fetchUsers()
+    this.props.fetchUsers(1)
   }
 
   render() {
-    const { users } = this.props
+    const { users, changePageItems, pageItems } = this.props
+
+    const selectHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      changePageItems(+e.target.value)
+    }
+
     return (
       <>
         <h1>Users</h1>
+        <select onChange={selectHandler}>
+          <option>10</option>
+        </select>
         <S.Users>
           {users
-            ? users.map((user) => {
+            ? users.map((user: any) => {
                 return (
                   <li key={user.id}>
                     <img src={user.img} alt="user-img" />
@@ -33,22 +42,27 @@ class Users extends React.Component<Props> {
               })
             : "no users"}
         </S.Users>
+        <Pagination pageItems={pageItems} />
       </>
     )
   }
 }
 
 const mapStateToProps = (state: AppRootState) => ({
-  users: state.users,
+  users: state.users.items,
+  pageItems: state.users.pageItems,
 })
 
 const mapDispatchToProps = {
-  fetchUsers: usersSlice.actions.fetchUsers,
+  fetchUsers: fetchUsers,
+  changePageItems: changePageItems,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users)
 
 type Props = {
   users: User[]
-  fetchUsers: any
+  fetchUsers: (currentPage: number) => void
+  pageItems: number
+  changePageItems: (pageItems: number) => ReturnType<typeof changePageItems>
 }
