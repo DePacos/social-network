a303fde1757878d8d332dfe9859b10ef1fed9491
DeBook <depacos1@gmail.com>
2024-09-02 23:18:28 +0300

    refactor for new syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App';
-import {BrowserRouter} from "react-router-dom";
-import './assets/fonts/fonts.css'
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import "./shared/assets/fonts/fonts.css"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import { router } from "./app/routers/routers"
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>
-);
-
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+)
