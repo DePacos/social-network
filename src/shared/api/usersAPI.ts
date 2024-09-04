e435d16a82d4250e83f742b5340747a2a8af946e
DeBook <depacos1@gmail.com>
2024-09-05 01:02:15 +0300

    added fetchProfile

--- src/shared/api/usersAPI.ts
+++ src/shared/api/usersAPI.ts
@@ -1,8 +1,11 @@
 import { instance } from "./instance"
-import { UsersResponse } from "../../app/types/types"
+import { UserProfile, User, UsersResponse } from "../../app/types/types"
 
 export const usersAPI = {
   fetchUsers(pageNumber: number = 1) {
     return instance.get<UsersResponse>(`users?page=${pageNumber}&count=${10}`)
+  },
+  fetchProfile(userId: number) {
+    return instance.get<UserProfile>(`profile/${userId}`)
   },
 }
