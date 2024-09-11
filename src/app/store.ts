4a432c6259d272532b573c42e74fd03cd782e24a
DeBook <depacos1@gmail.com>
2024-09-11 21:41:24 +0300

    added appReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,10 +6,12 @@ import {
 import { usersReducer } from "../entities/users/usersReducer"
 import { thunk } from "redux-thunk"
 import { profileReducer } from "../entities/users/profileReducer"
+import { appReducer } from "../entities/users/appReducer"
 
 export const rootReducer = combineReducers({
   users: usersReducer,
   profile: profileReducer,
+  app: appReducer,
 })
 
 export const store = legacy_createStore(
