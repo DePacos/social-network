66ddf5272be71a831b03a69005b0bce0895cf2f8
DeBook <depacos1@gmail.com>
2024-09-27 23:00:45 +0300

    added auth test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,8 @@ import { Container } from "../shared/ui/Container/Container"
 import { Header } from "../widgets/Header/Header"
 import { Sidebar } from "../widgets/Sidebar/Sidebar"
 import { Article } from "../widgets/Article/Article"
+import { authAPI } from "../shared/api/authAPI"
+import { Login } from "../features/auth/Login"
 
 type AppProps = {}
 
@@ -25,6 +27,24 @@ export class App extends React.Component<AppProps, { themeMode: boolean }> {
   }
 
   render() {
+    const me = () => {
+      authAPI.me().then((res) => console.log(res))
+    }
+
+    const logout = () => {
+      authAPI.logout().then((res) => console.log(res))
+    }
+
+    const login = () => {
+      authAPI
+        .login({
+          email: "depacos1@gmail.com",
+          password: "5285019",
+          rememberMe: true,
+        })
+        .then((res) => console.log(res))
+    }
+
     return (
       <div className="App">
         <ThemeProvider
@@ -32,10 +52,14 @@ export class App extends React.Component<AppProps, { themeMode: boolean }> {
         >
           <GlobalStyles />
           <Header changeTheme={this.changeTheme} />
+          <button onClick={() => login()}>Login</button>
+          <button onClick={() => me()}>Me</button>
+          <button onClick={() => logout()}>Logout</button>
           <main>
+            <Login />
             <MainWrap>
-              <Sidebar />
-              <Article />
+              {/*<Sidebar />*/}
+              {/*<Article />*/}
             </MainWrap>
           </main>
         </ThemeProvider>
