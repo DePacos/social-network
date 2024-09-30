f73d98a2e4c0295aefc7ebef6d5f1c2db8fbaa6c
DeBook <depacos1@gmail.com>
2024-09-30 23:43:14 +0300

    refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,15 @@ import styled, { ThemeProvider } from "styled-components"
 import { socialThemeDark } from "./styles/ThemeDark"
 import { socialThemeLight } from "./styles/ThemeLight"
 import { Container } from "../shared/ui/Container/Container"
-
-import { Header } from "../widgets/Header/Header"
+import Header from "../widgets/Header/Header"
 import { Sidebar } from "../widgets/Sidebar/Sidebar"
 import { Article } from "../widgets/Article/Article"
-import { authAPI } from "../shared/api/authAPI"
-import { Login } from "../features/auth/Login"
+import Login from "../features/auth/Login"
+import { AppRootState } from "./types/types"
+import { connect } from "react-redux"
 
-type AppProps = {}
-
-export class App extends React.Component<AppProps, { themeMode: boolean }> {
-  constructor(props: AppProps) {
+export class App extends React.Component<Props, { themeMode: boolean }> {
+  constructor(props: Props) {
     super(props)
 
     this.state = {
@@ -27,41 +25,25 @@ export class App extends React.Component<AppProps, { themeMode: boolean }> {
   }
 
   render() {
-    const me = () => {
-      authAPI.me().then((res) => console.log(res))
-    }
-
-    const logout = () => {
-      authAPI.logout().then((res) => console.log(res))
-    }
-
-    const login = () => {
-      authAPI
-        .login({
-          email: "depacos1@gmail.com",
-          password: "5285019",
-          rememberMe: true,
-        })
-        .then((res) => console.log(res))
-    }
-
     return (
       <div className="App">
         <ThemeProvider
           theme={this.state.themeMode ? socialThemeLight : socialThemeDark}
         >
           <GlobalStyles />
-          <Header changeTheme={this.changeTheme} />
-          <button onClick={() => login()}>Login</button>
-          <button onClick={() => me()}>Me</button>
-          <button onClick={() => logout()}>Logout</button>
-          <main>
+          {!this.props.isLoggedIn ? (
             <Login />
-            <MainWrap>
-              {/*<Sidebar />*/}
-              {/*<Article />*/}
-            </MainWrap>
-          </main>
+          ) : (
+            <>
+              <Header changeTheme={this.changeTheme} />
+              <main>
+                <MainWrap>
+                  <Sidebar />
+                  <Article />
+                </MainWrap>
+              </main>
+            </>
+          )}
         </ThemeProvider>
       </div>
     )
@@ -72,3 +54,13 @@ const MainWrap = styled(Container)`
   display: grid;
   grid-template-columns: 300px 1fr;
 `
+
+const mapStateToProps = (state: AppRootState) => ({
+  isLoggedIn: state.auth.isLoggedIn,
+})
+
+export default connect(mapStateToProps)(App)
+
+type Props = {
+  isLoggedIn: boolean
+}
