527b44a3e642cc64d2b81a7f0c36517aaa54fa29
DeBook <depacos1@gmail.com>
2024-09-30 23:41:25 +0300

    added isLoggedIn

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -4,42 +4,63 @@ import { Container } from "../../shared/ui/Container/Container"
 import logo from "../../shared/assets/images/logo.webp"
 import themeColor from "../../shared/assets/icons/theme-color.svg"
 import { Button } from "../../shared/ui/Button/Button"
+import { AppRootState } from "../../app/types/types"
+import { logout } from "../../entities/users/authReducer"
+import { connect } from "react-redux"
 
-type HeaderProps = {
-  changeTheme: () => void
+class Header extends React.Component<Props> {
+  render() {
+    const { changeTheme, isLoggedIn } = this.props
+    const logoutHandler = () => {
+      this.props.logout()
+    }
+
+    return (
+      <S.Header>
+        <Container>
+          <S.HeaderWrap>
+            <a href="/public#">
+              <img src={logo} alt="logo" />
+              <span>SocialNetwork</span>
+            </a>
+            <input
+              type="search"
+              name="search"
+              id="searchId"
+              placeholder="search"
+            />
+            <ul>
+              <li>
+                <Button children="RU" onClick={() => {}} />
+              </li>
+              <li>
+                <Button children="EN" onClick={() => {}} />
+              </li>
+            </ul>
+            <Button onClick={changeTheme}>
+              <img src={themeColor} alt="Change color mode" />
+            </Button>
+            {isLoggedIn && <Button children="LogOut" onClick={logoutHandler} />}
+          </S.HeaderWrap>
+        </Container>
+      </S.Header>
+    )
+  }
 }
 
-export const Header = ({ changeTheme }: HeaderProps) => {
-  const buttonHandler = () => {}
+const mapStateToProps = (state: AppRootState) => ({
+  isLoggedIn: state.auth.isLoggedIn,
+})
 
-  return (
-    <S.Header>
-      <Container>
-        <S.HeaderWrap>
-          <a href="/public#">
-            <img src={logo} alt="logo" />
-            <span>SocialNetwork</span>
-          </a>
-          <input
-            type="search"
-            name="search"
-            id="searchId"
-            placeholder="search"
-          />
-          <ul>
-            <li>
-              <Button children="RU" onClick={buttonHandler} />
-            </li>
-            <li>
-              <Button children="EN" onClick={buttonHandler} />
-            </li>
-          </ul>
-          <Button onClick={changeTheme}>
-            <img src={themeColor} alt="Change color mode" />
-          </Button>
-          <Button children="LogIn" onClick={buttonHandler} />
-        </S.HeaderWrap>
-      </Container>
-    </S.Header>
-  )
+const mapDispatchToProps = {
+  logout: logout,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
+
+type Props = {
+  changeTheme: () => void
+  logout: () => void
+  isLoading: boolean
+  isLoggedIn: boolean
 }
