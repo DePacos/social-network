16a3e19ea6a48f432264bc6b57e014ce21919e35
DeBook <depacos1@gmail.com>
2024-10-03 23:15:54 +0300

    fix styles

--- src/widgets/Sidebar/Sidebar_Styles.ts
+++ src/widgets/Sidebar/Sidebar_Styles.ts
@@ -1,67 +1,67 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
+const Sidebar = styled.aside`
+  padding: 40px 20px 20px 0;
+  border-right: ${({ theme }) => theme.colors.font} solid 1px;
+  height: 90vh;
 
-const Sidebar = styled.aside
-`
-    padding: 40px 20px 20px 0;
+  nav {
+    font-size: 18px;
 
-    nav {
-        font-size: 18px;
-
-        .active:after {
-            content: '';
-            display: inline-block;
-            position: absolute;
-            bottom: -5px;
-            left: 0;
-            background-color: currentColor;
-            height: 2px;
-            width: 100%;
-            border-radius: 10px;
-            transition: width 0.3s;
-        }
+    .active:after {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      bottom: -5px;
+      left: 0;
+      background-color: currentColor;
+      height: 2px;
+      width: 100%;
+      border-radius: 10px;
+      transition: width 0.3s;
     }
+  }
 
-    ul {
-        display: grid;
-        gap: 20px;
-        .active:after {
-            content: '';
-            display: inline-block;
-            position: absolute;
-            bottom: -5px;
-            left: 0;
-            background-color: currentColor;
-            height: 2px;
-            width: 100%;
-            border-radius: 10px;
-            transition: width 0.3s;
-        }
+  ul {
+    display: grid;
+    gap: 20px;
+    .active:after {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      bottom: -5px;
+      left: 0;
+      background-color: currentColor;
+      height: 2px;
+      width: 100%;
+      border-radius: 10px;
+      transition: width 0.3s;
     }
+  }
 
-    a {
-        position: relative;
-        padding: 0 2px;
-    }
+  a {
+    position: relative;
+    padding: 0 2px;
+  }
 
-    a:after {
-        content: '';
-        display: inline-block;
-        position: absolute;
-        bottom: -5px;
-        left: 0;
-        background-color: currentColor;
-        height: 2px;
-        width: 0;
-        border-radius: 10px;
-        transition: width 0.3s;
-    }
+  a:after {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    background-color: currentColor;
+    height: 2px;
+    width: 0;
+    border-radius: 10px;
+    transition: width 0.3s;
+  }
 
-    a:hover:after {
-        width: 100%;
-    }
+  a:hover:after {
+    width: 100%;
+  }
 `
 
 export const S = {
-    Sidebar,
+  Sidebar,
 }
