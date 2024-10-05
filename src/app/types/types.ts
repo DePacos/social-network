4bf04da4b5f99c16abcc6c69a26871b4c813688f
DePacos <depacos@yandex.ru>
2024-10-05 23:24:17 +0300

    added AppActions and AuthActions

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -7,8 +7,15 @@ import {
 import { rootReducer, store } from "../store"
 import { ThunkDispatch } from "redux-thunk"
 import { setProfile } from "../../entities/users/profileReducer"
-import { changeAppStatus } from "../../entities/users/appReducer"
-import { changeIsLoggedInStatus } from "../../entities/users/authReducer"
+import {
+  changeIsInitialized,
+  changeIsLoading,
+  setError,
+} from "../../entities/users/appReducer"
+import {
+  changeIsLoggedInStatus,
+  setCurrentUserId,
+} from "../../entities/users/authReducer"
 
 export type Photos = {
   small: string
@@ -66,7 +73,7 @@ export type MeData = {
 
 export type AuthResponse<Data = {}> = {
   resultCode: number
-  messages: []
+  messages: string[]
   data: Data
 }
 
@@ -77,8 +84,14 @@ export type LoginRequest = {
   captcha?: string
 }
 
-export type AppActions = ReturnType<typeof changeAppStatus>
-export type AuthActions = ReturnType<typeof changeIsLoggedInStatus>
+export type AppActions =
+  | ReturnType<typeof changeIsLoading>
+  | ReturnType<typeof changeIsInitialized>
+  | ReturnType<typeof setError>
+
+export type AuthActions =
+  | ReturnType<typeof changeIsLoggedInStatus>
+  | ReturnType<typeof setCurrentUserId>
 
 export type ProfileActions = ReturnType<typeof setProfile>
 
