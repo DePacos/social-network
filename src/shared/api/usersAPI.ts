2a1f50e7e375f76ffb9da8dd7e6cf097e6b2dee9
DePacos <depacos@yandex.ru>
2024-10-11 23:35:40 +0300

    added number of users per page to query

--- src/shared/api/usersAPI.ts
+++ src/shared/api/usersAPI.ts
@@ -2,8 +2,8 @@ import { instance } from "./instance"
 import { UserProfile, UsersResponse } from "../../app/types/types"
 
 export const usersAPI = {
-  fetchUsers(pageNumber: number = 1) {
-    return instance.get<UsersResponse>(`users?page=${pageNumber}&count=${10}`)
+  fetchUsers( pageNumber= 1 , countUsersInPage = 10) {
+    return instance.get<UsersResponse>(`users?page=${pageNumber}&count=${countUsersInPage}`)
   },
   fetchProfile(userId: number) {
     return instance.get<UserProfile>(`profile/${userId}`)
