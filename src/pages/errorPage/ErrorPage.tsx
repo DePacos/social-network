fe4c0b06c80fe8cb437410e7882ce829ccb09e39
DePacos <depacos@yandex.ru>
2024-11-17 11:17:52 +0300

    ref: imports

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,12 +1,14 @@
 import React from "react"
-import page404 from "../../shared/assets/images/404.webp"
-import { S } from "./ErrorPage_Styles"
+import page404 from "@/shared/assets/images/404.webp"
+import { S } from "./errorPage.styles"
 
-export const ErrorPage = () => {
-  return (
-    <S.Wrapper>
-      <h1>Sorry, page no found</h1>
-      <img alt='appError-page' src={page404}></img>
-    </S.Wrapper>
-  )
+export class ErrorPage extends React.Component {
+  render() {
+    return (
+      <S.Wrapper>
+        <h1>Sorry, page no found</h1>
+        <img alt='appError-page' src={page404}></img>
+      </S.Wrapper>
+    )
+  }
 }
