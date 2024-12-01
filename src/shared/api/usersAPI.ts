f9ecf50db959464bdc3f80b96130e38545cfcd77
DePacos <depacos@yandex.ru>
2024-12-02 00:28:25 +0300

    refactor

--- src/shared/api/usersAPI.ts
+++ src/shared/api/usersAPI.ts
@@ -1,15 +1,12 @@
 import { instance } from "./instance"
-import { UserProfile, UsersResponse } from "@/app/types/types"
+import { UsersResponse } from "@/app/types/types"
 
-export const usersAPI = {
+export const profileAPI = {
 
-  fetchUsers( pageNumber= 1 , countUsersInPage = 10) {
+  fetchUsers(pageNumber = 1, countUsersInPage = 10) {
     return instance.get<UsersResponse>(`users?page=${pageNumber}&count=${countUsersInPage}`)
   },
   searchUsers(filter: string) {
     return instance.get<UsersResponse>(`users?term=${filter}`)
-  },
-  fetchProfile(userId: number) {
-    return instance.get<UserProfile>(`profile/${userId}`)
-  },
+  }
 }
