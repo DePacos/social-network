8abe3574a6c7a8856b9abdfd58fde78980541cce
DePacos <depacos@yandex.ru>
2024-12-03 00:51:00 +0300

    refactor routing & added theme provider

--- src/app/routers/routers.tsx
+++ src/app/routers/routers.tsx
@@ -5,8 +5,25 @@ import { ErrorPage } from "@/pages/errorPage/ErrorPage"
 import Profile from "@/pages/profile/Profile"
 import Messages from "@/features/messages/Messages"
 import Users from "@/pages/users/Users"
+import EditProfile from "@/pages/profile/EditProfile"
+import Login from "@/features/auth/Login"
+import { ThemeProvider } from "styled-components"
+import { GlobalStyles } from "@/app/styles/global.styles"
+import { socialThemeDark } from "@/app/styles/themeDark.styles"
+import { manageThemeMode } from "@/app/utils/manageThemeMode"
+import { socialThemeLight } from "@/app/styles/themeLight.styles"
+
+const themeMode = manageThemeMode.get()
 
 export const router = createBrowserRouter([
+  { path: "/login",
+    element:(
+        <ThemeProvider theme={themeMode === "dark" ? socialThemeDark : socialThemeLight}>
+          <GlobalStyles/>
+          <Login />
+        </ThemeProvider>
+      )
+  },
   {
     path: "/",
     element: <App />,
@@ -27,6 +44,10 @@ export const router = createBrowserRouter([
       {
         path: "/profile/:id",
         element: <Profile />,
+      },
+      {
+        path: "/editprofile/:id",
+        element: <EditProfile />,
       },
       {
         path: "404",
