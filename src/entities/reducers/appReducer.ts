4777f4f3dfd9f71b7e809527337c0dc3a1d23113
DePacos <depacos@yandex.ru>
2024-12-09 01:06:14 +0300

    removed themeMode

--- src/entities/reducers/appReducer.ts
+++ src/entities/reducers/appReducer.ts
@@ -1,10 +1,8 @@
 import { AppActions, AppInitialState } from "@/app/types/types"
-import { manageThemeMode } from "@/app/utils/manageThemeMode"
 
 const initialState: AppInitialState = {
   isInitialized: false,
   isLoading: false,
-  themeMode: manageThemeMode.get() as 'dark' | 'light',
   appNotifications: "",
 }
 
@@ -16,8 +14,6 @@ export const appReducer = (state = initialState, action: AppActions) => {
       return { ...state, isInitialized: action.payload.status }
     case "SET_NOTIFICATION":
       return { ...state, appNotifications: action.payload.appNotifications }
-    case "SET_THEME_MODE":
-      return { ...state, themeMode: action.payload.themeMode }
     default:
       return state
   }
@@ -31,6 +27,3 @@ export const changeIsInitialized = (status: boolean) =>
 
 export const setError = (appNotifications: string) =>
   ({ type: "SET_NOTIFICATION", payload: { appNotifications } }) as const
-
-export const setThemeMode = (themeMode: 'dark' | 'light') =>
-  ({ type: "SET_THEME_MODE", payload: { themeMode } }) as const
