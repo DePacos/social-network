47242132223e479fa67d129d188dfb3ec73ea1fe
DePacos <depacos@yandex.ru>
2024-12-16 23:46:04 +0300

    refactor follow api

--- src/shared/api/followAPI.ts
+++ src/shared/api/followAPI.ts
@@ -1,13 +1,14 @@
 import { instance } from "@/shared/api/instance"
+import { Response } from "@/app/types/types"
 
 export const followAPI = {
   fetchFollow(userId: number) {
     return instance.get<boolean>(`/follow/${userId}`)
   },
   setFollow(userId: number) {
-    return instance.post(`/follow/${userId}`)
+    return instance.post<Response>(`/follow/${userId}`)
   },
   removeFollow(userId: number) {
-    return instance.delete(`/follow/${userId}`)
+    return instance.delete<Response>(`/follow/${userId}`)
   },
 }
