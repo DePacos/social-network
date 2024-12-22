d05d2a85bbeec1eb11b31ad2b0beec04ec052166
DePacos <depacos@yandex.ru>
2024-12-23 00:13:08 +0300

    added checkbox component

---
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,26 @@
+import { ComponentProps, forwardRef, ReactNode } from 'react'
+
+import { S } from './checkbox.styles'
+
+type Props = {
+  error?: string | undefined
+  label?: ReactNode | undefined
+} & ComponentProps<'input'>
+
+export const CheckBox = forwardRef<HTMLInputElement, Props>(
+  ({ disabled, error, id, label, ...props }, ref) => {
+    return (
+      <S.CheckboxWrap disabled={disabled}>
+        <input
+          type={'checkbox'}
+          ref={ref}
+          id={id}
+          disabled={disabled}
+          {...props}
+        />
+        {label && <label htmlFor={id}>{label}</label>}
+        {error && <span className={'error'}>{error}</span>}
+      </S.CheckboxWrap>
+    )
+  },
+)
---
+++ src/shared/ui/Checkbox/checkbox.styles.ts
@@ -0,0 +1,56 @@
+import check from '@/shared/assets/icons/check.svg'
+import styled, { css } from 'styled-components'
+
+const CheckboxWrap = styled.div<{
+  disabled?: boolean
+}>`
+  .error {
+    position: absolute;
+    display: block;
+    font-size: 14px;
+    padding: 0 0 0 25px;
+    color: red;
+  }
+
+  input {
+    display: none;
+    cursor: pointer;
+  }
+
+  input ~ label {
+    position: relative;
+    padding-left: 25px;
+    user-select: none;
+  }
+
+  input ~ label::after {
+    content: '';
+    position: absolute;
+    cursor: pointer;
+    left: 0;
+    top: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #fff;
+    border-radius: 5px;
+  }
+
+  input:checked ~ label::after {
+    content: '';
+    background: url(${check}) center #cb5b0f;
+  }
+
+  ${({ disabled }) => css`
+    ${disabled &&
+    css`
+      opacity: 0.7;
+      input ~ label::after {
+        cursor: default;
+      }
+    `}
+  `}
+`
+
+export const S = {
+  CheckboxWrap,
+}
