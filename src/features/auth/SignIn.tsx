31b2d58e5b7b76868ddd329392313699705f0d01
DePacos <depacos@yandex.ru>
2025-01-07 19:54:12 +0300

    feat: sighin

---
+++ src/features/auth/SignIn.tsx
@@ -0,0 +1,100 @@
+import { Loader, Mail } from 'lucide-react'
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+import { useAppDispatch, useAppSelector } from '@/app/hooks/stateHook'
+import {
+  selectAppIsLoading,
+  selectAppNotifications,
+} from '@/entities/reducers/appSlice'
+import {
+  selectAuthCaptcha,
+  selectAuthIsLoggedIn,
+} from '@/entities/reducers/authSlice'
+import { useSignIn } from '@/features/auth/modal/useSignIn'
+import logo from '@/shared/assets/images/logo.webp'
+import { Button } from '@/shared/ui/Button/Button'
+import { FormCheckbox } from '@/shared/ui/Checkbox/FormCheckbox'
+import { FormInput } from '@/shared/ui/Input/FormInput'
+import { Toast } from '@/shared/ui/Toast/Toast'
+
+import { S } from './signIn.styles'
+
+export const SignIn = () => {
+  const isLoading = useAppSelector(selectAppIsLoading)
+  const notifications = useAppSelector(selectAppNotifications)
+  const captcha = useAppSelector(selectAuthCaptcha)
+  const isLoggedIn = useAppSelector(selectAuthIsLoggedIn)
+  const dispatch = useAppDispatch()
+
+  const { control, handleSubmit, isValid, onSubmit } = useSignIn(dispatch)
+
+  if (isLoggedIn && !isLoading) {
+    return <Navigate to={'/'} />
+  }
+
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.Logo>
+          <img src={logo} alt={'logo'} />
+          <span>SocialNetwork</span>
+        </S.Logo>
+        <S.Form
+          error={notifications.type === 'appError' ? notifications.value : null}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <FormInput
+            control={control}
+            iconEnd={<Mail color={'black'} size={20} />}
+            placeholder={'Email*'}
+            name={'email'}
+            type={'email'}
+          />
+          <FormInput
+            control={control}
+            placeholder={'Password*'}
+            name={'password'}
+            type={'password'}
+          />
+          <S.WrapCheckbox>
+            <FormCheckbox
+              control={control}
+              id={'remember'}
+              name={'rememberMe'}
+              label={'Remember me'}
+            />
+          </S.WrapCheckbox>
+          {captcha && (
+            <>
+              <img src={captcha} alt={'captcha'} />
+              <FormInput
+                control={control}
+                placeholder={'enter captcha'}
+                name={'captcha'}
+              />
+            </>
+          )}
+          <Button
+            style={{ height: '46px' }}
+            endIcon={
+              isLoading ? (
+                <S.Animation>
+                  <Loader size={18} />
+                </S.Animation>
+              ) : null
+            }
+            variant={'primary'}
+            type={'submit'}
+            disabled={!isValid}
+          >
+            {isLoading ? 'Loading... ' : 'Log into your account'}
+          </Button>
+        </S.Form>
+      </S.Wrapper>
+      {notifications.type === 'catchError' && (
+        <Toast notification={notifications.value} />
+      )}
+    </S.Container>
+  )
+}
---
+++ src/features/auth/signIn.styles.ts
@@ -0,0 +1,87 @@
+import login from '@/shared/assets/images/login_bg.jpg'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  background:
+    ${({ theme }) => theme.colors.overlay},
+    url(${login}) no-repeat center;
+  background-size: cover;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 25px;
+  width: 320px;
+  min-height: 400px;
+  margin: 0 auto;
+  border: 2px solid;
+  border-radius: 10px;
+`
+const Form = styled.form<Props>`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+
+  button {
+    position: relative;
+    font-size: 14px;
+  }
+
+  button:last-of-type:before {
+    content: '${props => props.error}';
+    position: absolute;
+    top: -20px;
+    left: 5px;
+    font-size: 14px;
+    color: red;
+    width: max-content;
+  }
+`
+
+const Logo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  margin-bottom: 30px;
+`
+
+const WrapCheckbox = styled.div`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  font-size: 14px;
+`
+const Animation = styled.div`
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  animation: spin 3s linear infinite;
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+export const S = {
+  Animation,
+  Container,
+  Error,
+  Form,
+  Logo,
+  WrapCheckbox,
+  Wrapper,
+}
+
+type Props = {
+  error?: string | null
+}
