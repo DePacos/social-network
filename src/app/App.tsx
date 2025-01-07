7960b6ba5948d27e2300ce0e9f31d3ccc1ba37b5
DePacos <depacos@yandex.ru>
2025-01-07 19:51:36 +0300

    refactor & fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,90 +1,35 @@
-import React from "react"
-import { GlobalStyles } from "./styles/global.styles"
-import styled, { ThemeProvider } from "styled-components"
-import { socialThemeDark } from "./styles/themeDark.styles"
-import { socialThemeLight } from "./styles/themeLight.styles"
-import { ContainerStyles } from "@/shared/ui/Container/container.styles"
-import Header from "@/widgets/Header/Header"
-import { Sidebar } from "@/widgets/Sidebar/Sidebar"
-import { Article } from "@/widgets/Article/Article"
-import { AppRootState, ThemeModeContext } from "./types/types"
-import { connect } from "react-redux"
-import { me } from "@/entities/reducers/authReducer"
-import { changeIsLoading } from "@/entities/reducers/appReducer"
-import Skeleton from "react-loading-skeleton"
-import Toast from "@/shared/ui/Toast/Toast"
-import { Navigate } from "react-router-dom"
-import { ThemeContext } from "@/app/providers/ThemeContext"
+import React, { useEffect } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
-export class App extends React.Component<Props> {
-  static contextType = ThemeContext
-  context!: ThemeModeContext
+import { useAppDispatch, useAppSelector } from '@/app/hooks/stateHook'
+import { SkeletonStyled } from '@/app/styles/global.styles'
+import { selectAppIsInitialized } from '@/entities/reducers/appSlice'
+import { me, selectAuthIsLoggedIn } from '@/entities/reducers/authSlice'
+import { Header } from '@/widgets/Header/Header'
 
-  componentDidMount() {
-    this.props.me()
+export const App = () => {
+  const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(selectAuthIsLoggedIn)
+  const isInitialized = useAppSelector(selectAppIsInitialized)
+
+  useEffect(() => {
+    dispatch(me())
+  }, [])
+
+  if (!isInitialized) {
+    return <SkeletonStyled />
   }
 
-  renderContent() {
-    const { isLoggedIn, appError } = this.props
-
-    if (!isLoggedIn) {
-      return <Navigate to="/login" />
-    }
-
-    return (
-      <>
-        <Header />
-        <main>
-          <MainWrap>
-            <Sidebar />
-            <Article />
-          </MainWrap>
-          {appError && <Toast error={appError} />}
-        </main>
-      </>
-    )
+  if (!isLoggedIn) {
+    return <Navigate to={'/signin'} />
   }
 
-  render() {
-    const { isInitialized } = this.props
-
-    if (!isInitialized) {
-      return (
-        <div>
-          <Skeleton />
-        </div>
-      )
-    }
-
-    return (
-      <div className="App">
-        <ThemeProvider theme={this.context.themeMode === "light" ? socialThemeLight : socialThemeDark}>
-          <GlobalStyles />
-          {this.renderContent()}
-        </ThemeProvider>
-      </div>
-    )
-  }
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
 }
-
-const MainWrap = styled(ContainerStyles)`
-    display: grid;
-    grid-template-columns: 300px 1fr;
-`
-
-const mapStateToProps = (state: AppRootState) => ({
-  isLoggedIn: state.auth.isLoggedIn,
-  isInitialized: state.app.isInitialized,
-  appError: state.app.appNotifications,
-})
-
-type Props = {
-  isLoggedIn: boolean
-  isInitialized: boolean
-  appError: string
-  me: () => void
-}
-
-export default connect(mapStateToProps, {
-  me, changeIsLoading
-})(App)
--- src/shared/api/authAPI.ts
+++ src/shared/api/authAPI.ts
@@ -1,19 +1,20 @@
-import { instance } from "./instance"
-import { Response, LoginRequest, MeData } from "@/app/types/types"
+import { Response, LoginRequest, MeData } from '@/app/types/types'
+
+import { instance } from './instance'
 
 export const authAPI = {
-  me() {
-    return instance.get<Response<MeData>>(`/auth/me`)
+  captcha() {
+    return instance.get<{ url: string }>(`/security/get-captcha-url`)
   },
 
   login(data: LoginRequest) {
-    return instance.post<Response<{userId: number}>>(`/auth/login`, data)
+    return instance.post<Response<{ userId: number }>>(`/auth/login`, data)
   },
 
   logout() {
     return instance.delete<Response>(`/auth/login`)
   },
-  captcha() {
-    return instance.get<{url: string}>(`/security/get-captcha-url`)
+  me() {
+    return instance.get<Response<MeData>>(`/auth/me`)
   },
 }
